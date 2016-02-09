81d5fac8f2b8c0c2b39f413ea1bb55dc4347c3e3
Your Name <kienen.mason@gmail.com>
2016-02-09 14:05:19 -0800

    Started event app

--- rcannounce/settings.py
+++ rcannounce/settings.py
@@ -35,6 +35,13 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #Third Party apps
+    'crispy_forms',
+
+    #My apps
+    'event',
+
 )
 
 MIDDLEWARE_CLASSES = (
