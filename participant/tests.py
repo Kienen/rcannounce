720d14364d0f49c467fb7224a3e6fe0f951c4d96
Your Name <kienen.mason@gmail.com>
2016-02-10 13:58:22 -0800

    Updated login.html

--- participant/tests.py
+++ participant/tests.py
@@ -15,8 +15,10 @@ class TestHomePage(StaticLiveServerTestCase):
 
     #Billy Participant loads the home page
     def test_index_page(self):
-        response = self.browser.get('http://localhost:8000')
-        self.assertTemplateUsed(response, 'home.html')
+        self.browser.get(self.live_server_url)
+        self.assertTemplateUsed('home.html')
         self.assertIn('Events', self.browser.title)
+        #self.assertEqual(self.response_code, '200')
 
-        
+    #Billy creates an account
+    #def test_signup(self):    
